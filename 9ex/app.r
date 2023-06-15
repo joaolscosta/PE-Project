d6b530ca441f8675fecdc8150823afe3c7c7ec00
João Costa <joaoluissaraivacosta@gmail.com>
2023-06-15 11:20:51 +0100

    9ex TODO grafico

--- 9ex/app.r
+++ 9ex/app.r
@@ -1,3 +1,6 @@
+library(ggplot2)
+
+
 seed <- 1497
 n_values <- c(30,50,100,200,300,500,1000) # tamanho das amostras
 
@@ -13,9 +16,9 @@ set.seed(seed)
 # Gerar k = 2500 amostras de tamanho n para cada valor de n
 amostras <- list()
 for (n in n_values) {
-    # Gerar uma amostra de tamanho n
-    amostra <- rbinom(k, size = n, prob = p)
-    amostras[[as.character(n)]] <- amostra
+  # Gerar uma amostra de tamanho n
+  amostra <- rbinom(k, size = n, prob = p)
+  amostras[[as.character(n)]] <- amostra
 }
 
 # Metodo 1
@@ -23,40 +26,49 @@ z <- qnorm((1 + y) / 2)
 # ap^2 + bp + c = 0
 a <- 1
 calc_b = function (X,n) {
-    return(- ((z ^ 2) * (p * (1 - p) / n) + (2 * X)))
+  return(- ((z ^ 2) * (p * (1 - p) / n) + (2 * X)))
 }
 calc_method_1 = function (X, n) {
-    b <- calc_b(X, n) 
-    c <- X ^ 2
-    delta <- b ^ 2 - 4 * a * c
-    x1 <- (-b + sqrt(delta)) / (2 * a)
-    x2 <- (-b - sqrt(delta)) / (2 * a)
-    return(c(x1, x2))
+  b <- calc_b(X, n) 
+  c <- X ^ 2
+  delta <- b ^ 2 - 4 * a * c
+  x1 <- (-b + sqrt(delta)) / (2 * a)
+  x2 <- (-b - sqrt(delta)) / (2 * a)
+  return(c(x1, x2))
 }
 
 lista_diferenças <- list()
 # Calcular a diferença entre os metodos 1 e 2
 for (n in n_values) {
-    X <- mean(amostras[[as.character(n)]])
-
-    # Metodo 1
-    m1 <- calc_method_1(X,n)
-    comp_m1 <- abs(m1[2] - m1[1])
-    
-
-    # Metodo 2
-    desvio_padrao <- sd(amostras[[as.character(n)]])
-    valor_critico <- qnorm(1- (1-y)/2)
-    erro_padrao <- desvio_padrao / sqrt(n)
-    m2 <- c(X - valor_critico * erro_padrao,  X + valor_critico * erro_padrao)
-    comp_m2 <- abs(m2[2] - m2[1])
-    
-
-    diferença <- abs(comp_m1 - comp_m2)
-    lista_diferenças[[as.character(n)]] <- diferença
+  X <- mean(amostras[[as.character(n)]])
+  
+  # Metodo 1
+  m1 <- calc_method_1(X,n)
+  comp_m1 <- abs(m1[2] - m1[1])
+  
+  
+  # Metodo 2
+  desvio_padrao <- sd(amostras[[as.character(n)]])
+  valor_critico <- qnorm(1- (1-y)/2)
+  erro_padrao <- desvio_padrao / sqrt(n)
+  m2 <- c(X - valor_critico * erro_padrao,  X + valor_critico * erro_padrao)
+  comp_m2 <- abs(m2[2] - m2[1])
+  
+  
+  diferença <- abs(comp_m1 - comp_m2)
+  lista_diferenças[[as.character(n)]] <- diferença
 } 
 
 diferenças <- unlist(lista_diferenças)
 media_diff <- mean(diferenças)
 
-# 0.6345631
+df_diferencas <- data.frame(n = as.numeric(names(diferenças)),
+                            diferenca = unlist(lista_diferencas))
+
+grafico <- ggplot(df_diferencas, aes(x = n, y = diferenca)) +
+  geom_point() +
+  labs(x = "Tamanho da amostra (n)",
+       y = "Diferença entre os comprimentos dos intervalos",
+       title = "Diferença entre os comprimentos dos intervalos de confiança")
+
+print(grafico)
